cfd9c2ecd1ca2329360ae68b71e29f6ff1f9895e
Kevin Conner <connerk@gmail.com>
2015-08-05 21:18:02 -0400

    Planned animation.

---
+++ Code/AllYourFault/TimelineView.swift
@@ -0,0 +1,21 @@
+//
+//  TimelineView.swift
+//  AllYourFault
+//
+//  Created by Kevin Conner on 8/5/15.
+//  Copyright (c) 2015 Kevin Conner. All rights reserved.
+//
+
+import UIKit
+
+class TimelineView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
